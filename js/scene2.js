b50694b97e454294f34a4288d0ce6fb6fc156bce
soni17 <elb004@gmail.com>
2017-11-17 13:00:15 -0500

    scene-2 setting up stage

---
+++ js/scene2.js
@@ -0,0 +1,36 @@
+function scene2() {
+
+  // Setup Stage
+  stage = document.querySelector('#stage');
+  stageImg = stage.querySelector('#stage-img');
+  castle = document.querySelector('#castle');
+  castleImgSrc = castle.getAttribute('src');
+  stageImg.setAttribute('src', castleImgSrc);
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
